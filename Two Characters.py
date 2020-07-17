2d75b2611b8c66dd93cb5a413daeff9c546be469
Muralidharan Ganapathy <43808822+MuralidharanGanapathy@users.noreply.github.com>
2020-07-17 11:00:50 +0530

    Create Two Characters.py

---
+++ Two Characters.py
@@ -0,0 +1,30 @@
+def validate(string, char_set):
+    quo = len(string)//2
+    if len(string) % 2 == 0:
+        for i in char_set:
+            if "".join(i) * quo == string:
+                return len(string)
+    else:
+        for i in char_set:
+            #print(string[:len(string) - 1])
+            if "".join(i) * quo == string[:len(string) - 1] and string[0] == string[-1]:
+                return len(string)
+        
+    return 0
+
+def removal_set(string, rem_chars):
+    for i in rem_chars:
+        string = string.replace(i, "")
+    return string
+    
+from itertools import combinations, permutations
+inp_string = "beabeefeab"
+unique_characters = list(set(inp_string))
+rem_char_list = list(map(list, list(combinations(unique_characters, len(unique_characters) - 2))))
+char_set = list(map(list, list(permutations(unique_characters, 2))))
+maxi = 0
+for i in range(len(rem_char_list)):
+    length = validate(removal_set(inp_string, list(rem_char_list[i])), char_set)
+    if length > maxi:
+        maxi = length
+print(maxi)
