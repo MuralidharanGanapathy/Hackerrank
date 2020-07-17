4c847f66bd0691a438eaa4fa164ace193a352e38
Muralidharan Ganapathy <43808822+MuralidharanGanapathy@users.noreply.github.com>
2020-07-17 11:15:36 +0530

    Update Two Characters.py

--- Two Characters.py
+++ Two Characters.py
@@ -1,3 +1,4 @@
+
 def validate(string, char_set):
     quo = len(string)//2
     if len(string) % 2 == 0:
@@ -18,13 +19,17 @@ def removal_set(string, rem_chars):
     return string
     
 from itertools import combinations, permutations
-inp_string = "beabeefeab"
+input()
+inp_string = input()
 unique_characters = list(set(inp_string))
-rem_char_list = list(map(list, list(combinations(unique_characters, len(unique_characters) - 2))))
-char_set = list(map(list, list(permutations(unique_characters, 2))))
-maxi = 0
-for i in range(len(rem_char_list)):
-    length = validate(removal_set(inp_string, list(rem_char_list[i])), char_set)
-    if length > maxi:
-        maxi = length
-print(maxi)
+if len(unique_characters) > 1:
+    rem_char_list = list(map(list, list(combinations(unique_characters, len(unique_characters) - 2))))
+    char_set = list(map(list, list(permutations(unique_characters, 2))))
+    maxi = 0
+    for i in range(len(rem_char_list)):
+        length = validate(removal_set(inp_string, list(rem_char_list[i])), char_set)
+        if length > maxi:
+            maxi = length
+    print(maxi)
+else:
+    print(0)
